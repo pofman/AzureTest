39d08a366170803056eceef137ff6c5bb953152f
Pofman <g3xbetony@gmail.com>
2014-06-20 15:12:38 -0300

    Windows Phone app added!

---
+++ AzureTest.WP8.Client/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AzureTest.WP8.Client.Resources;
+
+namespace AzureTest.WP8.Client
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
